547131b5844e541e7b93148d9ba691daa11f832b
Sujin Kang <1214sujin@naver.com>
2024-11-25 16:17:57 +0900

    :hammer: [#3] fix: createAt, updatedAt 적용

--- src/main/java/myaong/popolog/portfolioservice/PortfolioServiceApplication.java
+++ src/main/java/myaong/popolog/portfolioservice/PortfolioServiceApplication.java
@@ -3,9 +3,11 @@ package myaong.popolog.portfolioservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class PortfolioServiceApplication {
 
     public static void main(String[] args) {
