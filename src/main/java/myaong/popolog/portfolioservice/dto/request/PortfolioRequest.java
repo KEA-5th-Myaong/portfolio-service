885bcdfa9f5d54780d4642c6a70b623307186373
Sujin Kang <1214sujin@naver.com>
2024-11-25 16:22:24 +0900

    :hammer: [#3] fix: 포트폴리오 생성 시 직렬화 오류 수정

--- src/main/java/myaong/popolog/portfolioservice/dto/request/PortfolioRequest.java
+++ src/main/java/myaong/popolog/portfolioservice/dto/request/PortfolioRequest.java
@@ -1,10 +1,12 @@
 package myaong.popolog.portfolioservice.dto.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import myaong.popolog.portfolioservice.dto.PortfolioContentDTO;
 
 @Getter
+@JsonSerialize(as = PortfolioContentDTO.class)
 public class PortfolioRequest extends PortfolioContentDTO {
 
 	@NotBlank(message = "포트폴리오 제목을 입력해주세요.")
